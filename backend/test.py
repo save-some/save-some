ceab8ccb0ff0501b5cfd94ce66250392abc91440
shale <slucas001@citymail.cuny.edu>
2026-03-17 13:00:29 -0400

    added a basic version of an example query to walmart rapid api but it doesn't work

---
+++ backend/test.py
@@ -0,0 +1,18 @@
+import requests
+import os
+
+value = os.getenv('API')
+
+url = "https://walmart-data.p.rapidapi.com/walmart-category.php"
+
+querystring = {"url":"https://www.walmart.com/browse/electronics/3944"}
+
+headers = {
+	"x-rapidapi-key": value,
+	"x-rapidapi-host": "walmart-data.p.rapidapi.com"
+}
+
+response = requests.get(url, headers=headers, params=querystring)
+
+print(response.status_code)
+print(response.json())
