7c782f9773cd7507303c2c6b37f8f4e821b21cb4
shale <slucas001@citymail.cuny.edu>
2026-04-09 16:11:58 -0400

    have the starting code for statefulwidgets for account, history, maps, and proudcts pages

---
+++ frontend/save-some-ui/lib/screens/account.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class AccountScreen extends StatefulWidget {
+  const AccountScreen({super.key});
+
+  @override
+  State<AccountScreen> createState() => _AccountScreenState();
+}
+
+class _AccountScreenState extends State<AccountScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
