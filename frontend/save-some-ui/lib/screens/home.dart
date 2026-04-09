f1de22947353d71e7fc46a30d58364b4ffc27eb9
shale <slucas001@citymail.cuny.edu>
2026-04-09 16:08:38 -0400

    added the contennt for the home screen, bottom nar bar and links to other pages

--- frontend/save-some-ui/lib/screens/home.dart
+++ frontend/save-some-ui/lib/screens/home.dart
@@ -1,13 +1,86 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+import 'package:flutter_svg/flutter_svg.dart';
+
+import 'package:save_some_ui/screens/products.dart';
+import 'package:save_some_ui/screens/account.dart';
+import 'package:save_some_ui/screens/history.dart';
+import 'package:save_some_ui/screens/maps.dart';
+
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  static const List<Widget> _pages = <Widget>[
+    ProductScreen(),
+    MapsScreen(),
+    HomeScreen(),
+    HistoryScreen(),
+    AccountScreen(),
+  ];
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+
+  void onItemTapped(int idx) {
+    setState(() {
+      _selectedIndex = idx;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text('you are signed in'),
+        child: Column(
+          children: [
+            const Placeholder(),
+            BottomNavigationBar(
+              unselectedItemColor: Colors.grey,
+              selectedItemColor: Colors.black,
+              currentIndex: _selectedIndex,
+              backgroundColor: Colors.black,
+              onTap: onItemTapped,
+              items: [
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.shop),
+                  label: 'Products',
+                ),
+
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.location_on),
+                  label: 'Maps',
+                ),
+
+                BottomNavigationBarItem(
+                  icon: SvgPicture.asset(
+                    'assets/wallet-logo.svg',
+                    height: 30,
+                    width: 30,
+                    placeholderBuilder: (_) => const SizedBox(
+                      height: 25,
+                      width: 25,
+                      child: CircularProgressIndicator(strokeWidth: 3),
+                    ),
+                  ),
+                  label: 'Home',
+                ),
+
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.folder),
+                  label: 'History',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.account_circle),
+                  label: 'Account',
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
