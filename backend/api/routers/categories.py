0d64985618aa8791f32c547860310fd918e75fe4
shale <slucas001@citymail.cuny.edu>
2026-08-16 01:03:09 -0400

    fixed import statements + added list categories endpoint

--- backend/api/routers/categories.py
+++ backend/api/routers/categories.py
@@ -1,9 +1,21 @@
-from fastapi import APIRouter
-from helpers.db import retrieve_all_retailers
+from fastapi import APIRouter, HTTPException, Query
+from helpers.db import (
+    retrieve_categories_for_retailer,
+    retrieve_all_categories,
+    retrieve_all_canonical_categories
+)
 from api.models import Category, Product, Retailer, Store, User
-
+from typing import Optional, List, Dict
+from api.utils import get_db_handle
 
 router = APIRouter (
     prefix = "/categories",
     tags = ["categories"]
 )
+
+
+ 
+@router.get("/", response_model = List[Category])
+def list_categories():
+    with get_db_handle() as conn:
+        return retrieve_all_canonical_categories(conn)
