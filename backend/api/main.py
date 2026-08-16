4c40f7ad756500ac73e8785f6dd87a9f1eec613f
shale <slucas001@citymail.cuny.edu>
2026-08-15 22:53:38 -0400

    cors, and fixing import statements

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,10 +1,24 @@
-from fastapi import fastapi
-from routers import categories, retailers, users, products
-from models import Category, Product, Retailer, Store, User
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from api.routers import categories, retailers, users, products
+from api.models import Category, Product, Retailer, Store, User
 from datetime import datetime
 
-application = FastAPI(
+
+origins = [
+    "http://localhost"
+]
+
+application = FastAPI (
     prefix = "/v1"
+)
+
+application.add_middleware (
+    CORSMiddleware,
+    allow_origins = origins,
+    allow_credentials = True,
+    allow_methods = ["*"], 
+    allow_headers = ["*s"],
 )
 
 uptime = datetime.now()
